e8d2ec0c7c4b46741f465e336f934c5b48208ec5
ahmetatakan <ahmetatakan79@gmail.com>
2018-01-08 14:52:37 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name='django-entegrator2',
+    version='0.1',
+    packages=['entegrator'],
+    description='Entegrator',
+    author='ahmetatakan',
+    author_email='ahmetatakan79@gmail.com',
+	classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 2.0',  # replace "X.Y" as appropriate
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',  # example license
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
